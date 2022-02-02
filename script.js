d0166641666b60954c207316a0144d52f89dfa51
GwynethJoy17 <78654856+GwynethJoy17@users.noreply.github.com>
2022-02-01 21:52:13 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var map = L.map('map').setView([37.7, -122.4], 10);
+var Stamen_Terrain = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+$.getJSON('https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson',function(data){
+    var ratIcon = L.icon({
+      iconUrl: 'https://img.icons8.com/color/50/000000/baby-yoda.png',
+      iconSize: [30,30]
+    });
+    var rodents = L.geoJson(data,{
+      pointToLayer: function(feature,latlng){
+        var marker = L.marker(latlng,{icon: ratIcon});
+        marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.OPEN_DT + '<br/>' + feature.properties.SUBJECT);
+        return marker;
+      }
+    });
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(rodents);
+    map.addLayer(clusters);
+});
